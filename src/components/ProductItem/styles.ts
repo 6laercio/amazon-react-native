6c0a399ef16a3a496d233fb21184ddd65ba941c8
Laercio Nascimento <laercio@protonmail.ch>
2021-08-07 07:27:34 -0300

    add styles to ProductItem component

---
+++ src/components/ProductItem/styles.ts
@@ -0,0 +1,42 @@
+import {StyleSheet} from 'react-native';
+
+const styles = StyleSheet.create({
+  root: {
+    flexDirection: 'row',
+    borderWidth: 1,
+    borderColor: '#d1d1d1',
+    borderRadius: 10,
+    backgroundColor: '#fff',
+    marginVertical: 5,
+  },
+  image: {
+    flex: 2,
+    height: 150,
+    resizeMode: 'contain',
+  },
+  rightContainer: {
+    padding: 10,
+    flex: 3,
+  },
+  title: {
+    fontSize: 18,
+  },
+  ratingContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    marginVertical: 10,
+  },
+  star: {
+    margin: 2,
+  },
+  price: {
+    fontWeight: 'bold',
+  },
+  oldPrice: {
+    fontSize: 12,
+    fontWeight: 'normal',
+    textDecorationLine: 'line-through',
+  },
+});
+
+export default styles;
