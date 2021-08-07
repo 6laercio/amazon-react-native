1602504894cd64d291af06471b9901d17ea93a30
Laercio Nascimento <laercio@protonmail.ch>
2021-08-07 00:29:37 -0300

    remove rule from eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,8 +2,6 @@ module.exports = {
   root: true,
   extends: '@react-native-community',
   rules: {
-    // Turn off rules that are no longer necessary in React 17
-    'react/react-in-jsx-scope': 'off',
     // It is not necessary to use prop types with TypeScript
     'react/prop-types': 'off',
     'react/require-default-props': 'off',
